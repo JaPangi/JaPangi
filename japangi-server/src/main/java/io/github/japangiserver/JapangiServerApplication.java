0953625484e924a84d291c7b85533d9a02a1ee22
wwan13 <wwan13@naver.com>
2024-03-26 15:34:19 +0900

    feat : init spring boot project

---
+++ japangi-server/src/main/java/io/github/japangiserver/JapangiServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.japangiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapangiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapangiServerApplication.class, args);
+	}
+
+}
---
+++ japangi-server/src/test/java/io/github/japangiserver/JapangiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.japangiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapangiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
