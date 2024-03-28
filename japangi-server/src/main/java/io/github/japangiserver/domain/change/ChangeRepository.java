5a577d317f8c680a70bcc1af31e4d4d29a45320b
JunRock <wnstjr120422@naver.com>
2024-03-28 13:02:55 +0900

    feat : 음료 모듈

--- japangi-server/src/main/java/io/github/japangiserver/domain/change/ChangeRepository.java
+++ japangi-server/src/main/java/io/github/japangiserver/domain/change/ChangeRepository.java
@@ -2,5 +2,9 @@ package io.github.japangiserver.domain.change;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ChangeRepository extends JpaRepository<Change, Long> {
+    List<Change> findAllByVendingMachineVendingMachineId(Long vendingMachineId);
 }
---
+++ japangi-server/src/main/java/io/github/japangiserver/domain/drink/DrinkService.java
@@ -0,0 +1,10 @@
+package io.github.japangiserver.domain.drink;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DrinkService {
+    private final DrinkRepository drinkRepository;
+}
