b20ccfbccddf21a8a0c1efc6a5e3345bd753ccf0
JunRock <wnstjr120422@naver.com>
2024-03-31 14:59:51 +0900

    feat : 자판기 현재 재고상황 로직

--- japangi-server/src/main/java/io/github/japangiserver/domain/vendingmachine/VendingMachineService.java
+++ japangi-server/src/main/java/io/github/japangiserver/domain/vendingmachine/VendingMachineService.java
@@ -3,10 +3,13 @@ package io.github.japangiserver.domain.vendingmachine;
 import io.github.japangiserver.domain.change.Change;
 import io.github.japangiserver.domain.change.ChangeRepository;
 import io.github.japangiserver.domain.drink.DrinkRepository;
+import io.github.japangiserver.domain.drink.DrinkStatus;
 import io.github.japangiserver.domain.money.MoneyRepository;
 import io.github.japangiserver.domain.order.OrderTarget;
 import io.github.japangiserver.domain.stock.Stock;
 import io.github.japangiserver.domain.stock.StockRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class VendingMachineService {
+
     private final static int DEFAULT_AMOUNT = 10;
 
     private final VendingMachineRepository vendingMachineRepository;
@@ -27,35 +31,34 @@ public class VendingMachineService {
         VendingMachine vendingMachine = new VendingMachine();
         Long vendingMachineId = vendingMachineRepository.save(vendingMachine).getVendingMachineId();
         drinkRepository.findAll()
-                .forEach(drink -> {
-                    Stock stock = Stock.builder()
-                            .amount(DEFAULT_AMOUNT)
-                            .vendingMachine(vendingMachine)
-                            .drink(drink)
-                            .build();
-                    stockRepository.save(stock);
-                });
+            .forEach(drink -> {
+                Stock stock = Stock.builder()
+                    .amount(DEFAULT_AMOUNT)
+                    .vendingMachine(vendingMachine)
+                    .drink(drink)
+                    .build();
+                stockRepository.save(stock);
+            });
 
         moneyRepository.findAll()
-                .forEach(money -> {
-                    Change change = Change.builder()
-                            .vendingMachine(vendingMachine)
-                            .amount(DEFAULT_AMOUNT)
-                            .money(money)
-                            .build();
-                    changeRepository.save(change);
-                });
+            .forEach(money -> {
+                Change change = Change.builder()
+                    .vendingMachine(vendingMachine)
+                    .amount(DEFAULT_AMOUNT)
+                    .money(money)
+                    .build();
+                changeRepository.save(change);
+            });
         return vendingMachineId;
     }
 
-    public void checkStock(OrderTarget orderTarget){
-        Stock stock = stockRepository.findByDrinkDrinkIdAndVendingMachineVendingMachineId(
-                        orderTarget.drinkId(),
-                        orderTarget.vendingMachineId()
-                )
-                .orElseThrow(() -> new IllegalStateException("존재하지 않는 음료입니다"));
-
-        if(stock.getAmount()==0)
-            throw new IllegalStateException("재고가 부족합니다.");
+    public VendingMachineStatus getCurrentStatus(long vendingMachineId) {
+        List<Stock> stocks = stockRepository.findByVendingMachineVendingMachineId(
+            vendingMachineId
+        );
+        List<DrinkStatus> statusList = stocks.stream()
+            .map(DrinkStatus::from)
+            .toList();
+        return new VendingMachineStatus(statusList);
     }
 }
--- japangi-server/src/main/java/io/github/japangiserver/domain/vendingmachine/VendingMachineStatus.java
+++ japangi-server/src/main/java/io/github/japangiserver/domain/vendingmachine/VendingMachineStatus.java
@@ -1,6 +1,10 @@
 package io.github.japangiserver.domain.vendingmachine;
 
-public record VendingMachineStatus(
+import io.github.japangiserver.domain.drink.DrinkStatus;
+import java.util.List;
 
+public record VendingMachineStatus(
+    List<DrinkStatus> drinkStatuses
 ) {
+
 }
