4d4081f3754f00352a8210affe1d2872a913a997
JunRock <wnstjr120422@naver.com>
2024-04-01 16:33:19 +0900

    feat : 자판기에 있는 잔돈 확인

--- japangi-server/src/main/java/io/github/japangiserver/domain/change/ChangeProvider.java
+++ japangi-server/src/main/java/io/github/japangiserver/domain/change/ChangeProvider.java
@@ -15,32 +15,32 @@ public class ChangeProvider {
 
     /**
      * 거스름돈을 거슬러 주는 메소드
-     * @param excess 거슬러줘야 하는 금액
+     *
+     * @param excess  거슬러줘야 하는 금액
      * @param changes 자판기가 보유하고 있는 거스름돈의 목록
      * @return 거스름돈
      */
     public MoneyAmounts provide(int excess, List<Change> changes) {
-
         changes.sort(Comparator.comparing(change -> change.getMoney().getMoneyId()));
 
         AtomicInteger remainingChanges = new AtomicInteger(excess);
         List<MoneyAmount> moneyAmounts = new ArrayList<>();
-
-        IntStream.range(changes.size(), 0)
-                .forEach(i -> {
-                    int value = changes.get(i).getMoney().getValue();
-                    int changeAmount = remainingChanges.get() / value;
-
-                    if (changeAmount > 0) {
-                        remainingChanges.addAndGet(-changeAmount * value);
-                        MoneyAmount amount = MoneyAmount.builder()
-                                .amount(changeAmount)
-                                .value(value)
-                                .build();
-                        moneyAmounts.add(amount);
-                    }
-                });
-
+        IntStream.range(0, changes.size())
+            .map(i -> changes.size() - i-1)
+            .forEach(i -> {
+                int value = changes.get(i).getMoney().getValue();
+                int changeAmount = remainingChanges.get() / value;
+                changes.get(i).validChanges(changeAmount); //자판기가 거스러줄 잔돈이 있는지 확인
+                if (changeAmount > 0) {
+                    remainingChanges.addAndGet(-changeAmount * value);
+                    MoneyAmount amount = MoneyAmount.builder()
+                        .amount(changeAmount)
+                        .value(value)
+                        .build();
+                    moneyAmounts.add(amount);
+                    changes.get(i).remainChange(changeAmount);
+                }
+            });
         return new MoneyAmounts(moneyAmounts);
     }
 }
