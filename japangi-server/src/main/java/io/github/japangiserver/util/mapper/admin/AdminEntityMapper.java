2d79f579188a59193b1672a0c882b050e19481ef
JunRock <wnstjr120422@naver.com>
2024-06-10 12:35:02 +0900

    feat : mapper 패키지 주석

--- japangi-server/src/main/java/io/github/japangiserver/util/mapper/admin/AdminEntityMapper.java
+++ japangi-server/src/main/java/io/github/japangiserver/util/mapper/admin/AdminEntityMapper.java
@@ -3,7 +3,15 @@ package io.github.japangiserver.util.mapper.admin;
 import io.github.japangiserver.domains.admin.Admin;
 import io.github.japangiserver.domains.admin.persistence.entity.AdminEntity;
 
+/** NOTE
+ * 관리자 entity mapper
+ */
 public class AdminEntityMapper {
+
+    /** NOTE
+     * domain -> entity 로 매핑
+     * @param domain admin domain
+     */
     public static AdminEntity toEntityFromDomain(Admin domain) {
         return AdminEntity.builder()
             .username(domain.username())
