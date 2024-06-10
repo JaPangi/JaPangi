fc3ceeec9ab7ddced69b739f3c6f64ead6134e3e
JunRock <wnstjr120422@naver.com>
2024-06-10 12:33:40 +0900

    feat : dateformat 패키지 주석

--- japangi-server/src/main/java/io/github/japangiserver/util/datefomat/DateFormatter.java
+++ japangi-server/src/main/java/io/github/japangiserver/util/datefomat/DateFormatter.java
@@ -2,11 +2,21 @@ package io.github.japangiserver.util.datefomat;
 
 import java.time.format.DateTimeFormatter;
 
+/** NOTE
+ * 날짜 형식 지정 유틸 클래스
+ */
 public class DateFormatter {
+
+    /** NOTE
+     * 년.월.일 형식 포맷팅
+     */
     public static DateTimeFormatter getDateFormatter() {
         return DateTimeFormatter.ofPattern("yyyy-MM-dd");
     }
 
+    /** NOTE
+     * 년.월 형식 포맷팅
+     */
     public static DateTimeFormatter getMonthFormatter() {
         return DateTimeFormatter.ofPattern("yyyy-MM");
     }
