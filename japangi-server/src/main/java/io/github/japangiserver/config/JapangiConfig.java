8574d4665ae9d9829b178b2083d8855c56298300
JunRock <wnstjr120422@naver.com>
2024-06-10 12:31:26 +0900

    feat : socket config 패키지 주석

--- japangi-server/src/main/java/io/github/japangiserver/config/JapangiConfig.java
+++ japangi-server/src/main/java/io/github/japangiserver/config/JapangiConfig.java
@@ -6,12 +6,19 @@ import io.wwan13.dispatchersorvlet.configuration.support.SocketServerPropertiesR
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
+/** NOTE
+ * Socket 연결 설정
+ */
 @Configuration
 @EnableSocketServer
 public class JapangiConfig implements SocketServerConfigurer {
 
     @Value(value = "${spring.socket.port}")
     private int socketPort;
+
+    /** NOTE
+     * Socket 포트 지정
+     */
     @Override
     public void setup(SocketServerPropertiesRegistry registry) {
         registry
