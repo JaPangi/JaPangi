18460ba4f4499ad9a4f91e762c6c0e9c21aa99e1
wwan13 <wwan13@naver.com>
2024-06-12 16:27:15 +0900

    refactor : 환경변수 사용

--- japangi-relay/src/main/java/io/github/japangirelay/socketclient/SocketClientConfig.java
+++ japangi-relay/src/main/java/io/github/japangirelay/socketclient/SocketClientConfig.java
@@ -1,12 +1,25 @@
 package io.github.japangirelay.socketclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SocketClientConfig {
+
+    private final String host;
+    private final int port;
+
+    public SocketClientConfig(
+            @Value("${socket.client.host}") String host,
+            @Value("${socket.client.port}") int port
+    ) {
+        this.host = host;
+        this.port = port;
+    }
+
     @Bean
     public SocketClient socket() {
-        return SocketClient.of("127.0.0.1", 8070);
+        return SocketClient.of(host, port);
     }
 }
